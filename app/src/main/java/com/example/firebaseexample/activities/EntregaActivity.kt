cd366330eee184ef6d2ce4fcf01f718ab9861f0c
brennoharten <brennoharten@gmail.com>
2020-12-14 01:17:57 -0300

    empacou aqui

---
+++ app/src/main/java/com/example/firebaseexample/activities/EntregaActivity.kt
@@ -0,0 +1,12 @@
+package com.example.firebaseexample.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.firebaseexample.R
+
+class EntregaActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_entrega)
+    }
+}
---
+++ app/src/main/java/com/example/firebaseexample/models/User.kt
@@ -0,0 +1,10 @@
+package com.example.firebaseexample.models
+
+data class User (
+        val email: String = "",
+        val password: String = "",
+        val name: String = "",
+        val score: Int = 0,
+        val code: String = ""
+
+        )
